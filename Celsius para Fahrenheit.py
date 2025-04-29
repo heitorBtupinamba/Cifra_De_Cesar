5c24b8737e742c6d44eabe3eaf72849f874db99d
Heitor Botelho <heitorbotelhotupi@gmail.com>
2025-04-28 21:21:23 -0300

    Delete Celsius para Fahrenheit.py

--- Celsius para Fahrenheit.py
+++
@@ -1,9 +0,0 @@
-print(" PROGRAMA DE CONVERSÃO CELSIUS PARA FAHRENHEIT")
-print()
-
-celsius=float(input(" Digite a temperatura em graus °C (Celsius): "))
-print()
-
-fahrenheit=  (9 * celsius + 160) / 5
-
-print(f" Essa temperatura em Fahrenheit é: {fahrenheit:.1f}°F.")
